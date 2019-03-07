4f0835cab6a1a13ee1c17fb7fb7b5ec3ef7cc5a0
Egg Sandwich <eggsandwich@Eggs-MacBook-Pro.local>
2019-03-07 13:28:22 -0700

    move effect hook and fetch request from Account to DreamArchive

--- src/components/DreamArchive/index.js
+++ src/components/DreamArchive/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { AuthUserContext, withAuthorization } from '../Session';
 
 
 const ArchivePage = () => (
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/test')
+      .then(response => response.json())
+      .then((myJson) => {
+        console.log(myJson);
+      })
+  }),
+
   <AuthUserContext.Consumer>
     {authUser => (
       <div>
