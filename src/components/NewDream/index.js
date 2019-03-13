5ad6c201572de0c183dd551694e833dc5113ebde
megalobamboozlementarianism <abe.coley@gmail.com>
2019-03-13 16:50:29 -0600

    set up keyword parsing and pixabay requests

--- src/components/NewDream/index.js
+++ src/components/NewDream/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
 
+
 import { withAuthorization } from '../Session';
 import * as ROUTES from '../../Constants/routes';
+
+import { commonWords, keyWords } from './archetypes';
 
 const { REACT_APP_BACKEND_URL } = process.env;
 
@@ -13,31 +16,95 @@ class NewDreamPage extends Component {
     content:'',
     _id: '',
     userId: this.props.firebase.auth.O,
+    imgArr: [],
   }
 
-  handleChange = (event) => {  
+  handleChange = (event) => {
     event.preventDefault();
     event.stopPropagation();
     this.setState({[event.target.name]: event.target.value});
   }
 
+  // add img urls to state
+
+  urlsToState = (url) =>{
+    let thumbsArr = this.state.imgArr.slice();
+    thumbsArr.push(url);
+    this.setState({imgArr: thumbsArr});
+  }
+
   addDream = (e) => {
     e.preventDefault();
-    const { title, content, userId } = this.state;
+    const self = this;
+    const { title, content, userId, imgArr } = this.state;
     if (!title || !content) {
       return;
     }
+
+    //get just the unique words
+    let lower = content.toLowerCase();
+    lower = lower.replace(/[^\w\d ]/g, '');
+    let dreamWords = lower.split(' ')
+    dreamWords = dreamWords.filter(function (word) {
+      return commonWords.indexOf(word) === -1;
+    });
+
+    // match against archetypes
+    for (let i = 0; i < dreamWords.length; i++){
+      let word = dreamWords[i];
+      if (keyWords.includes(word)){
+        buildCompleteURL(word);
+        // const pTag = document.createElement('p');
+        // pTag.textContent = word;
+        // document.getElementById('image-container').append(pTag);
+      }
+    }
+
+    // build pixabay search URL
+    function buildCompleteURL(searchValue) {
+      const baseURL = 'https://pixabay.com/api/?key=11543969-d5ffb78383da99ab7336a1888';
+      const imageType = '&image_type=photo&pretty=true';
+      const searchTerm = '&q=' + searchValue;
+      const completeURL = baseURL + searchTerm + imageType;
+      callPixa(completeURL);
+    }
+
+    // call pixabay
+    function callPixa(url) {
+      console.log(self);
+      fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        self.urlsToState(data.hits[0].previewURL)
+        //appendImg(data.hits[0])
+      });
+    }
+
+    // add pixabay results to page
+    function appendImg(imageObject) {
+      const htmlTemplate =`
+          <div>
+              <img src="${imageObject.previewURL}" alt="..." >
+          </div>
+        `
+      const imgDiv = document.createElement('div');
+      imgDiv.innerHTML = htmlTemplate;
+      document.getElementById('image-container').append(imgDiv);
+    }
+
+    // Post to DB
     if(title){
+      console.log("state imgarr ", imgArr);
       fetch(`${REACT_APP_BACKEND_URL}/dreams`, {
         method: "POST",
-        body: JSON.stringify({ title, content, userId }),
+        body: JSON.stringify({ title, content, userId, imgArr }),
         headers: {
           "Content-Type": "application/json"
         }
       })
       .then(response => response.json())
       .then(() => {
-        this.props.history.push(ROUTES.DREAM_ARCHIVE);
+        // this.props.history.push(ROUTES.DREAM_ARCHIVE);
       });
     }
   }
@@ -70,10 +137,18 @@ class NewDreamPage extends Component {
         <br/>
         <SaveButton name="addDream" onClick={ (e) => {this.addDream(e)}}>Save</SaveButton>
         </form>
+        <ThumbsDiv id='image-container'></ThumbsDiv>
       </PageStyle>
     );
   }
 }
+
+const ThumbsDiv = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 5px;
+`
 
 const PageStyle = styled.div`
   margin-left: 25px;
