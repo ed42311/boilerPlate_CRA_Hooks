fd64c1b58da398ba287fa7744d6695ebb28fd2b1
megalobamboozlementarianism <abe.coley@gmail.com>
2019-03-18 16:58:35 -0600

    styled the dream archive page with our color blob, added pics

--- src/components/DreamArchive/index.js
+++ src/components/DreamArchive/index.js
@@ -22,33 +22,49 @@ class ArchivePage extends Component {
       .then(response => response.json())
       .then((myJson) => {
         this.setState({dreams: myJson});
+        console.log(this.state);
       })
+
   }
 
   render() {
+
     return(
       <PageStyle>
+        <BlobInputContainerS>
+          <ColorBlob/>
+        </BlobInputContainerS>
         <BlobInputContainerS>
           <ColorBlob/>
         </BlobInputContainerS>
         <AuthUserContext.Consumer>
           {authUser => (
             <div>
-              <h1>user ID: {authUser.uid}</h1>
               <h1 id="test-dreamarchive-user-h1">Dream Archive for {authUser.email}</h1>
               {this.state.dreams.map( (dream) =>
                 <DreamDiv key={dream._id} >
-                  <h2>{dream.title}</h2>
-                  <p>{dream.content}</p>
-                  <Link to={{
-                    pathname: './editDream',
-                    state: {
-                      title: dream.title,
-                      content: dream.content,
-                      _id: dream._id,
-                      userId: dream.userId,
+                  <TitleRowDiv>
+                    <h2>{dream.title}</h2>
+                    <Link to={{
+                      pathname: './editDream',
+                      state: {
+                        title: dream.title,
+                        content: dream.content,
+                        _id: dream._id,
+                        userId: dream.userId,
+                      }
+                    }}>Edit Dream</Link>
+                  </TitleRowDiv>
+                  <StyledHR />
+                  <ContentRowDiv>
+                    <p>{dream.content}</p>
+                  </ContentRowDiv>
+                  <StyledHR />
+                  <ImgRowDiv>
+                    {dream.images.length &&
+                      dream.images.map( (image) => <StyledImg src={image.url} key={image._id}/>)
                     }
-                  }}>Edit Dream</Link>
+                  </ImgRowDiv>
                 </DreamDiv>
               )}
             </div>
@@ -59,12 +75,46 @@ class ArchivePage extends Component {
   }
 }
 
+const StyledImg = styled.img`
+  margin: 10px;
+  border-radius: 15px;
+  opacity: 0.75;
+  -webkit-box-shadow: 2px 2px 3px 1px rgba(181,181,181,0.26);
+  -moz-box-shadow: 2px 2px 3px 1px rgba(181,181,181,0.26);
+  box-shadow: 2px 2px 3px 1px rgba(181,181,181,0.26);
+  &:hover{
+    transition: 1s ease-in-out;
+    opacity: 1.0;
+  }
+`
+
+const StyledHR = styled.hr`
+  border: 0.5px solid rgba(0,0,0,.1);
+  width: 100%;
+`
+
+const TitleRowDiv = styled.div`
+  display: flex;
+  justify-content: inherit;
+`
+
+const ContentRowDiv = styled.div`
+`
+
+const ImgRowDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
 const PageStyle = styled.div`
   margin-left: 25px;
 `
 
 const DreamDiv = styled.div`
-  width: 75%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 60%;
   padding: 15px;
   border-radius: 1em 5em 1em 5em / 2em 1em 2em 1em;
   margin-bottom: 25px;
