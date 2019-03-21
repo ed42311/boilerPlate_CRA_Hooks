7c83cb4fc9a49aee5bc7b378f9a32c282c37502d
megalobamboozlementarianism <abe.coley@gmail.com>
2019-03-20 20:26:26 -0600

    fixed image toggle, also fixed duplicate images mapping into image container

--- src/components/NewOrEditDream/ImagesContainer.js
+++ src/components/NewOrEditDream/ImagesContainer.js
@@ -5,20 +5,30 @@ import styled, { css } from "styled-components";
 function ImageContainer({url, selected, toggleSelected, saveCaption, caption}) {
 
   return (
-    <ThumbDiv selected={selected} onClick={(e) => toggleSelected(e, url)}>
-      <ImageCaptioning src={url} alt="..."></ImageCaptioning>
-      {(selected) ?
+    <ThumbDiv selected={selected} >
+      {(selected) &&
+        <div>
+          <ImageCaptioning
+          src={url}
+          alt="..."
+        />
+        <ButtonS onClick={(e) => toggleSelected(e, url)}>X</ButtonS>
         <CaptionInput
           autoFocus="autofocus"
           defaultValue={caption}
           onKeyUp={(e) => e.keyCode === 13 && e.target.blur()}
           onBlur={(e) => saveCaption(e, url)}>
-        </CaptionInput> :
-        null
+        </CaptionInput>
+        </div>
       }
     </ThumbDiv>
   )
 }
+const ButtonS = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+`
 
 const CaptionInput = styled.input`
   background-color: grey;
@@ -35,6 +45,8 @@ const ImageCaptioning = styled.img`
 
 const ThumbDiv = styled.div`
   ${props => props.selected && css`
+    position: relative;
+    display: inline-block;
     background-color: grey;
     padding: 3px 3px 3px 3px;
     font-size: 80%;
